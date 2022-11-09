773ea9bee3b02c4bacf18bba98840fc5ff33a9fa
ThoradTM <pheonixgamming@gmail.com>
2022-11-09 00:12:18 -0600

    Item Items

--- src/main/java/net/thorad/testmod/TestMod.java
+++ src/main/java/net/thorad/testmod/TestMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.thorad.testmod.item.TestItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -26,7 +27,7 @@ public class TestMod
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
+        TestItems.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/thorad/testmod/item/TestItems.java
@@ -0,0 +1,22 @@
+package net.thorad.testmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.thorad.testmod.TestMod;
+
+public class TestItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> NAQUADAH = ITEMS.register("naquadah",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
