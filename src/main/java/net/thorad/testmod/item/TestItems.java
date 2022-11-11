45ddd54bbe6ccf6956b195af48bd37be587b5820
ThoradTM <pheonixgamming@gmail.com>
2022-11-10 23:38:34 -0600

    added eight ball

--- src/main/java/net/thorad/testmod/item/TestItems.java
+++ src/main/java/net/thorad/testmod/item/TestItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.thorad.testmod.TestMod;
+import net.thorad.testmod.item.custom.EightBallItem;
 
 public class TestItems
 {
@@ -15,6 +16,9 @@ public class TestItems
 
     public static final RegistryObject<Item> NAQUADAH = ITEMS.register("naquadah",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB).stacksTo(1)));
     public static void register(IEventBus eventBus)
     {
         ITEMS.register(eventBus);
